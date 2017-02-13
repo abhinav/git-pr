a911c875238d5e75d304e1e60c5a496ff3788ad1
Akshay Shah <akshayjshah@users.noreply.github.com>
2017-02-13 10:12:16 -0800

    Support the one true heir of vi (#6)
    The King lives.

--- editor/pick.go
+++ editor/pick.go
@@ -3,7 +3,7 @@ package editor
 // Pick an editor based on the given name.
 func Pick(name string) (Editor, error) {
 	switch name {
-	case "vi", "vim", "nvim", "mvim", "gvim":
+	case "vi", "vim", "nvim", "mvim", "gvim", "elvis":
 		return NewViLike(name)
 	default:
 		return NewBasic(name)
