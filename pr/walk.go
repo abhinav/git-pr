c439b675af7744ff47754ef64385d9a450a65a90
Abhinav Gupta <mail@abhinavg.net>
2017-05-21 14:35:58 -0400

    pr: Implement a concurrent traversal (#51)
    This adds a `pr.Walk` function to traverse one or more PRs and their
    children concurrently in an unspecified order.

---
+++ pr/walk.go
@@ -0,0 +1,173 @@
+package pr
+
+import (
+	"container/list"
+	"fmt"
+	"runtime"
+	"sync"
+
+	"go.uber.org/multierr"
+
+	"github.com/google/go-github/github"
+)
+
+// Visitor defines what to do at each pull request during a walk.
+type Visitor interface {
+	// Visits the given pull request and returns a new visitor to visit its
+	// children.
+	//
+	// If a visitor was not returned, the children of this PR will not be
+	// visited.
+	//
+	// This function MAY be called concurrently. Implementations MUST be
+	// thread-safe.
+	Visit(*github.PullRequest) (Visitor, error)
+}
+
+//go:generate mockgen -package=prtest -destination=prtest/mocks.go github.com/abhinav/git-fu/pr Visitor
+
+// WalkConfig configures a pull request traversal.
+type WalkConfig struct {
+	// Maximum number of pull requests to visit at the same time.
+	//
+	// Defaults to the number of CPUs available to this process.
+	Concurrency int
+
+	// Children retrieves the children of the given pull request.
+	//
+	// The definition of what constitutes a child of a PR is left up to the
+	// implementation.
+	//
+	// This function MAY be called concurrently. Implementations MUST be
+	// thread-safe.
+	Children func(*github.PullRequest) ([]*github.PullRequest, error)
+}
+
+// Walk traverses a pull request tree by visiting the given pull requests and
+// their children in an unspecified order. The only ordering guarantee is that
+// parents are visited before their children.
+//
+// Errors encountered while visiting pull requests are collatted and presented
+// as one.
+func Walk(cfg WalkConfig, pulls []*github.PullRequest, v Visitor) error {
+	if cfg.Concurrency <= 0 {
+		cfg.Concurrency = runtime.NumCPU()
+	}
+
+	w := walker{
+		// TODO: Magic number. Should make this customizable or leave it the
+		// same as Concurrency.
+		tasks:    make(chan task, 8),
+		children: cfg.Children,
+	}
+
+	w.ongoing.Add(len(pulls))
+	go func() {
+		// If pulls contains more than 8 items, we don't want to block on
+		// filling tasks just yet.
+		for _, pr := range pulls {
+			w.tasks <- task{PR: pr, Visitor: v}
+		}
+	}()
+
+	for i := 0; i < cfg.Concurrency; i++ {
+		go w.Worker()
+	}
+	w.ongoing.Wait()
+	close(w.tasks)
+
+	return multierr.Combine(w.errors...)
+}
+
+// Request to visit a single pull request with a specific visitor.
+type task struct {
+	PR      *github.PullRequest
+	Visitor Visitor
+}
+
+type walker struct {
+	// Incoming tasks. Any worker can handle these.
+	tasks chan task
+
+	// Number of ongoing tasks.
+	ongoing sync.WaitGroup
+
+	children func(*github.PullRequest) ([]*github.PullRequest, error)
+
+	// Errors encountered while processing.
+	errorsMu sync.Mutex
+	errors   []error
+}
+
+func (w *walker) Worker() {
+	// Walker-local buffer for incoming tasks that should be pushed into
+	// w.tasks when it's empty.
+	taskBuffer := list.New()
+
+worker:
+	for {
+	fill:
+		// Exhaust as much of the buffer as we can.
+		for taskBuffer.Len() > 0 {
+			e := taskBuffer.Front()
+			select {
+			case w.tasks <- e.Value.(task):
+				taskBuffer.Remove(e)
+			default:
+				// No more room in channel.
+				break fill
+			}
+		}
+
+		t, ok := <-w.tasks
+		if !ok {
+			// Channel closed. We're done.
+			break worker
+		}
+
+		newTasks, err := w.visit(t)
+		if err != nil {
+			w.errorsMu.Lock()
+			w.errors = append(w.errors, err)
+			w.errorsMu.Unlock()
+		} else if len(newTasks) > 0 {
+			for _, task := range newTasks {
+				taskBuffer.PushBack(task)
+			}
+		}
+		w.ongoing.Add(len(newTasks) - 1)
+	}
+}
+
+func (w *walker) visit(t task) (_ []task, err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			if e, ok := x.(error); ok {
+				err = e
+			} else {
+				// TODO: log the panic
+				err = fmt.Errorf("panic: %v", x)
+			}
+		}
+	}()
+
+	v, err := t.Visitor.Visit(t.PR)
+	if err != nil {
+		return nil, err
+	}
+
+	if v == nil {
+		return nil, nil
+	}
+
+	children, err := w.children(t.PR)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := make([]task, len(children))
+	for i, pr := range children {
+		tasks[i] = task{PR: pr, Visitor: v}
+	}
+	return tasks, nil
+}
---
+++ pr/walk_test.go
@@ -0,0 +1,301 @@
+package pr_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/abhinav/git-fu/pr"
+	"github.com/abhinav/git-fu/pr/prtest"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockChildren struct {
+	ctrl *gomock.Controller
+}
+
+func newMockChildren(ctrl *gomock.Controller) *mockChildren {
+	return &mockChildren{ctrl: ctrl}
+}
+
+func (m *mockChildren) Expect(pr interface{}) *gomock.Call {
+	return m.ctrl.RecordCall(m, "Call", pr)
+}
+
+func (m *mockChildren) Call(pr *github.PullRequest) (out []*github.PullRequest, err error) {
+	results := m.ctrl.Call(m, "Call", pr)
+	out, _ = results[0].([]*github.PullRequest)
+	err, _ = results[1].(error)
+	return
+}
+
+func TestWalk(t *testing.T) {
+	type children map[int][]int
+
+	type visit struct {
+		// Whether the children of this node should be visited.
+		VisitChildren bool
+
+		// If non-nil, this visit will fail with an error.
+		Error error
+
+		// If non-nil, this visit will panic with the given value.
+		Panic interface{}
+	}
+
+	type visits map[int]visit
+
+	concurrency := []int{0, 1, 2, 4, 8}
+
+	tests := []struct {
+		Desc  string
+		Pulls []int
+
+		Children children
+		Visits   map[int]visit
+
+		WantErr []string
+	}{
+		{Desc: "empty", Pulls: []int{}},
+		{
+			Desc:     "single",
+			Pulls:    []int{1},
+			Children: children{1: {}},
+			Visits:   visits{1: {VisitChildren: true}},
+		},
+		{
+			Desc:    "single error",
+			Pulls:   []int{1},
+			Visits:  visits{1: {Error: errors.New("great sadness")}},
+			WantErr: []string{"great sadness"},
+		},
+		{
+			Desc:  "single panic error",
+			Pulls: []int{1},
+			Visits: visits{
+				1: {Panic: errors.New("great sadness")},
+			},
+			WantErr: []string{"great sadness"},
+		},
+		{
+			Desc:  "single panic",
+			Pulls: []int{1},
+			Visits: visits{
+				1: {Panic: "great sadness"},
+			},
+			WantErr: []string{"panic: great sadness"},
+		},
+		{
+			Desc:   "single no children",
+			Pulls:  []int{1},
+			Visits: visits{1: {}},
+		},
+		{
+			Desc:  "single hop",
+			Pulls: []int{1, 2, 3},
+			Children: children{
+				1: {4, 5},
+				2: {},
+				4: {},
+				5: {},
+			},
+			Visits: visits{
+				1: {VisitChildren: true},
+				2: {VisitChildren: true},
+				3: {},
+				4: {VisitChildren: true},
+				5: {VisitChildren: true},
+			},
+		},
+		{
+			Desc:  "multi hop",
+			Pulls: []int{1, 2},
+			Children: children{
+				1:  {3},
+				2:  {4, 5},
+				3:  {},
+				4:  {6},
+				5:  {7},
+				6:  {8, 9},
+				7:  {},
+				8:  {10},
+				9:  {},
+				10: {11},
+				11: {},
+			},
+			Visits: visits{
+				1:  {VisitChildren: true},
+				2:  {VisitChildren: true},
+				3:  {VisitChildren: true},
+				4:  {VisitChildren: true},
+				5:  {VisitChildren: true},
+				6:  {VisitChildren: true},
+				7:  {VisitChildren: true},
+				8:  {VisitChildren: true},
+				9:  {VisitChildren: true},
+				10: {VisitChildren: true},
+				11: {VisitChildren: true},
+			},
+		},
+		{
+			Desc:  "multi hop errors",
+			Pulls: []int{1, 2},
+			Children: children{
+				1: {3},
+				2: {4, 5},
+				4: {6},
+				5: {7},
+				7: {},
+			},
+			Visits: visits{
+				1: {VisitChildren: true},
+				2: {VisitChildren: true},
+				3: {VisitChildren: true, Error: errors.New("something went wrong")},
+				4: {VisitChildren: true},
+				5: {VisitChildren: true},
+				6: {VisitChildren: true, Error: errors.New("great sadness")},
+				7: {VisitChildren: true},
+			},
+			WantErr: []string{"great sadness", "something went wrong"},
+		},
+		{
+			Desc:  "channel overflow",
+			Pulls: []int{1, 21},
+			Children: children{
+				1: {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+				2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {}, 10: {},
+				11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {},
+				21: {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40},
+				22: {}, 23: {}, 24: {}, 25: {}, 26: {}, 27: {}, 28: {}, 29: {}, 30: {},
+				31: {}, 32: {}, 33: {}, 34: {}, 35: {}, 36: {}, 37: {}, 38: {}, 39: {}, 40: {},
+			},
+			Visits: visits{
+				1:  {VisitChildren: true},
+				2:  {VisitChildren: true},
+				3:  {VisitChildren: true},
+				4:  {VisitChildren: true},
+				5:  {VisitChildren: true},
+				6:  {VisitChildren: true},
+				7:  {VisitChildren: true},
+				8:  {VisitChildren: true},
+				9:  {VisitChildren: true},
+				10: {VisitChildren: true},
+				11: {VisitChildren: true},
+				12: {VisitChildren: true},
+				13: {VisitChildren: true},
+				14: {VisitChildren: true},
+				15: {VisitChildren: true},
+				16: {VisitChildren: true},
+				17: {VisitChildren: true},
+				18: {VisitChildren: true},
+				19: {VisitChildren: true},
+				20: {VisitChildren: true},
+				21: {VisitChildren: true},
+				22: {VisitChildren: true},
+				23: {VisitChildren: true},
+				24: {VisitChildren: true},
+				25: {VisitChildren: true},
+				26: {VisitChildren: true},
+				27: {VisitChildren: true},
+				28: {VisitChildren: true},
+				29: {VisitChildren: true},
+				30: {VisitChildren: true},
+				31: {VisitChildren: true},
+				32: {VisitChildren: true},
+				33: {VisitChildren: true},
+				34: {VisitChildren: true},
+				35: {VisitChildren: true},
+				36: {VisitChildren: true},
+				37: {VisitChildren: true},
+				38: {VisitChildren: true},
+				39: {VisitChildren: true},
+				40: {VisitChildren: true},
+			},
+		},
+	}
+
+	for _, conc := range concurrency {
+		for _, tt := range tests {
+			name := fmt.Sprintf("concurrency=%v/%v", conc, tt.Desc)
+			t.Run(name, func(t *testing.T) {
+				ctrl := gomock.NewController(t)
+				defer ctrl.Finish()
+
+				getChildren := newMockChildren(ctrl)
+				for parent, children := range tt.Children {
+					getChildren.
+						Expect(prMatcher{Number: parent}).
+						Return(fakePullRequests(children), nil)
+				}
+
+				visitor := prtest.NewMockVisitor(ctrl)
+				for num, visit := range tt.Visits {
+					var v pr.Visitor
+					if visit.VisitChildren {
+						v = visitor
+					}
+
+					call := visitor.EXPECT().Visit(prMatcher{Number: num})
+					switch {
+					case visit.Error != nil:
+						call.Return(v, visit.Error)
+					case visit.Panic != nil:
+						p := visit.Panic
+						call.Do(func(*github.PullRequest) { panic(p) }).Return(v, nil)
+					default:
+						call.Return(v, nil)
+					}
+				}
+
+				cfg := pr.WalkConfig{
+					Children:    getChildren.Call,
+					Concurrency: conc,
+				}
+				err := pr.Walk(cfg, fakePullRequests(tt.Pulls), visitor)
+
+				if len(tt.WantErr) > 0 {
+					if !assert.Error(t, err) {
+						return
+					}
+
+					for _, msg := range tt.WantErr {
+						assert.Contains(t, err.Error(), msg)
+					}
+					return
+				}
+
+				assert.NoError(t, err)
+			})
+		}
+	}
+}
+
+type prMatcher struct {
+	Number int
+}
+
+var _ gomock.Matcher = prMatcher{}
+
+func (m prMatcher) String() string {
+	return fmt.Sprintf("pull request #%v", m.Number)
+}
+
+func (m prMatcher) Matches(x interface{}) bool {
+	pr, ok := x.(*github.PullRequest)
+	if !ok {
+		return false
+	}
+
+	return pr.GetNumber() == m.Number
+}
+
+func fakePullRequests(nums []int) []*github.PullRequest {
+	prs := make([]*github.PullRequest, len(nums))
+	for i, n := range nums {
+		prs[i] = &github.PullRequest{Number: github.Int(n)}
+	}
+	return prs
+}
